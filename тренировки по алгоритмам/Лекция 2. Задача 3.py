06e3065f252e6b85bbaad682f6e068fcb3e56803
Ekaterina Kozyreva <83861300+E-Kozyreva@users.noreply.github.com>
2022-11-23 22:28:09 +0300

    delete files

--- тренировки по алгоритмам/Лекция 2. Задача 3.py
+++
@@ -1,8 +0,0 @@
-# Дана последовательность чисел длиной N (N > 0). Найти максимальное число в последовательности.
-
-def findmax(seq):
-  ans = seq[0]
-  for i in range(len(seq)):
-    if seq[i] > ans:
-      ans = seq[i]
-  return ans
